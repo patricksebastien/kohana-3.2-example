d0b8e552de24d2e9d9e4997b02cefddf63e434cf
Patrick Sebastien <patrick@11h11.com>
2011-12-16 08:58:58 -0500

    add empty directory

--- application/config/database.php
+++ application/config/database.php
@@ -19,9 +19,9 @@ return array
 			 * Ports and sockets may be appended to the hostname.
 			 */
 			'hostname'   => 'localhost',
-			'database'   => 'ko32example',
-			'username'   => 'ko32example',
-			'password'   => 'k1k2k3k4',
+			'database'   => '',
+			'username'   => '',
+			'password'   => '',
 			'persistent' => FALSE,
 		),
 		'table_prefix' => '',
